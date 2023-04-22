fb79b84b108614743f8b84f4b7951e95a1d69ac0
Josias Kasongo <josiaskas@gmail.com>
2023-04-22 19:40:15 -0400

    add host for external connection

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000
+    host: '0.0.0.0',
+    port: 3000,
   },
   plugins: [vue(), vueJsx()],
   resolve: {
