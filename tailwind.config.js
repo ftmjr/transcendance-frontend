adc2f93ee7953e871a97f59ec2066f2a670eea9a
Francis Lahoud <flahoud@student.42quebec.com>
2023-05-13 16:29:36 -0400

    update file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "src/**/*.vue", "src/App.vue"
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
+
