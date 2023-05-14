b4b304a3c42e01008dedbaa0a2179b22244dd682
Francis Lahoud <97901448+FrancisL93@users.noreply.github.com>
2023-05-13 22:17:52 -0400

    Update LoginView.vue

--- src/LoginView.vue
+++ src/LoginView.vue
@@ -28,8 +28,8 @@
           method: 'POST',
           body: new URLSearchParams({
             'grant_type': 'client_credentials',
-            'client_id': 'u-s4t2ud-618818154b26420d4cb2bdcfb8a4b9d532d60f5daa6dc2e51152cb506324549b',
-            'client_secret': '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'
+            'client_id': '',
+            'client_secret': ''
           })
         }).then(response => response.json()).then(res => console.log(res.data)).catch(e => console.log(e.message))
       },
