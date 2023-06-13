a0e5507d83f07901b17e41348aa9654c038221ff
gederooney <gyebadokpo61@gmail.com>
2023-06-13 12:03:23 -0400

    first cofiguration of the app. Added husky for pre-commit  hooks. added tailwindcss and removed the boostraped files. Also added sass for scss handling.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
+  <nav-bar />
   <main>
-    <TheWelcome />
+    <div class="container">Welcome home</div>
   </main>
 </template>
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue'
+
+const NavBar = defineAsyncComponent(() => import('@/components/Header.vue'))
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    NavBar
+  },
+  data() {
+    return {}
+  }
+})
+</script>
