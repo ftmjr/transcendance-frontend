3bcd299af2155f01a7d345cf5bc3356f41d6aaa1
RONY GEDEON YEBADOKPO <49495160+Gederooney@users.noreply.github.com>
2023-06-13 16:59:09 -0400

    issue #5 (#8)
    * Start homepage with login button - Rony
    
    * update file
    
    * Update LoginView.vue
    
    * Update LoginView.vue
    
    * Change main.css to base.css
    
    * Link to LoginView
    
    * Unnecessary file
    
    * first cofiguration of the app. Added husky for pre-commit  hooks. added tailwindcss and removed the boostraped files. Also added sass for scss handling.
    
    * added ignore deprecation warning to tsconfig.* files
    
    * Added vite to the latest version
    
    * removed package-lock.json
    
    ---------
    
    Co-authored-by: Francis Lahoud <flahoud@student.42quebec.com>
    Co-authored-by: Francis Lahoud <97901448+FrancisL93@users.noreply.github.com>

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header>
+    <div class="container">
+      <div>
+        <h1>Logo</h1>
+      </div>
+      <div>
+        <div class="container mx-auto inl">
+          <ul>
+            <li v-for="item in menu" :key="item.name">
+              <router-link :to="item.link">{{ item.name }}</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { RouterLink } from 'vue-router'
+
+export default defineComponent({
+  name: 'nav-bar',
+  components: {
+    RouterLink
+  },
+  data() {
+    const menu = [
+      {
+        name: 'Accueil',
+        link: '/'
+      },
+      {
+        name: 'Profile',
+        link: '/profile'
+      }
+    ]
+    return {
+      menu
+    }
+  }
+})
+</script>
