74971d533c458fc8a00b50d92c13127b6faa7337
Rurikide <79031388+Rurikide@users.noreply.github.com>
2023-06-23 10:25:29 -0400

    test input regex

---
+++ src/components/testInput/Index.vue
@@ -0,0 +1,19 @@
+Vue.createApp({
+    data: () => ({ email: 'john@gmail.com' }),
+    computed: {
+      isValidEmail() {
+        return /^[^@]+@\w+(\.\w+)+\w$/.test(this.email);
+      }
+    },
+    template: `
+<div>
+      <div>
+        <input v-model="email" />
+      </div>
+<div>
+        Email is {{isValidEmail ? 'valid' : 'invalid'}}
+      </div>
+    </div>
+    `
+  }).mount('#example');
+  
