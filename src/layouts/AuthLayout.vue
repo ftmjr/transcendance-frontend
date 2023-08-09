8eb0c818c353f18afad887e6c038562805a3a070
Josias Kasongo <josiaskas@gmail.com>
2023-08-09 18:58:18 -0400

    set ath layout to be able to change image

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -2,10 +2,9 @@
   <main class="w-screen h-screen overflow-hidden">
     <div class="grid w-full h-full grid-cols-12 bg-darkBlue">
       <aside class="relative hidden h-full lg:block lg:col-span-8">
-        <img src="../assets/images/authBg.png" alt="landing" class="object-cover w-full h-full" />
-        <div
-          class="absolute top-0 left-0 z-10 grid w-full h-full bg-gradient place-items-center"
-        ></div>
+        <img :src="bgImage" alt="landing" class="object-cover w-full h-full" />
+<!--        <div v-show="routeName !=='reset-password'" class="absolute top-0 left-0 z-10 grid w-full h-full bg-gradient place-items-center">-->
+<!--        </div>-->
       </aside>
       <section
         class="relative grid h-full col-span-12 overflow-scroll lg:col-span-4 bg-darkBlue place-items-center box-border"
@@ -18,11 +17,22 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import assetBg from '@/assets/images/authBg.png'
 
 export default defineComponent({
   data() {
     return {
-      bgImages: ['/assets/images/authBg.png', '/pong/backgrounds/pink_pong_bg.png']
+      bgImages: [assetBg, '/pong/backgrounds/pink_pong_bg.png']
+    }
+  },
+  computed: {
+    routeName(): string {
+      return this.$route.name as string
+    },
+    bgImage(): string {
+      if (this.routeName === 'auth') return this.bgImages[0]
+      else if (this.routeName === 'reset-password') return this.bgImages[1]
+      else return this.bgImages[0]
     }
   }
 })
