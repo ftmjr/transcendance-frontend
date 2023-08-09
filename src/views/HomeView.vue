85ea6911ee5be9cc95e168529770c5bfbad186df
Josias Kasongo <josiaskas@gmail.com>
2023-08-09 18:30:49 -0400

    use authStore in setup to have access to reactivity in the right way.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,17 @@
 <template>
-  <nav-bar />
-  <main>
-    <div class="container"></div>
-  </main>
+  <div class="container"></div>
 </template>
+
 <script lang="ts">
-import { defineAsyncComponent, defineComponent } from 'vue'
+import { defineComponent } from 'vue'
 import useAuthStore from '@/stores/AuthStore'
 
-const NavBar = defineAsyncComponent(() => import('@/components/Header.vue'))
-
 export default defineComponent({
-  name: 'home-view',
-  components: {
-    NavBar
-  },
-  created() {
-    if (!useAuthStore().isAuthenticated) this.$router.push('/auth')
+  name: 'DashBoard',
+  components: {},
+  setup() {
+    const authStore = useAuthStore()
+    return { authStore }
   },
   data() {
     return {}
