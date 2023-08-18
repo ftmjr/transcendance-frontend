8f99744300343fd4335edd8efbe072157b524659
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-18 03:04:40 -0400

    functional chat general

--- src/views/ChatViewTest.vue
+++ src/views/ChatViewTest.vue
@@ -5,109 +5,130 @@
         <v-img :src="user.profile.avatar"></v-img>
       </v-avatar>
     </aside>
-    <aside class="select-container">
-      <v-select
-          v-if="chatRooms.length"
-          v-model="currentRoom.name"
-          :items="chatRooms.map(room => room.name)"
-          label="Select a Chat Room"
-          item-text="name"
-          bg-color="background"
-      ></v-select>
+    <aside class="chatRoom-select-container">
+    <v-select
+        bg-color='background'
+        v-model="select"
+        :items="chatRooms"
+        item-title="name"
+        label="Select"
+        persistent-hint
+        return-object
+        single-line
+    ></v-select>
     </aside>
-    <v-btn color="background" variant="tonal" @click="openCreateRoomDialog">
-      Create Chat Room
-    </v-btn>
-    <!--    pop up create Room-->
-    <v-dialog v-model="createRoomDialog" max-width="500px">
-      <v-card>
-        <v-card-title>Create Chat Room</v-card-title>
-        <v-card-text>
-          <v-text-field label="Chatroom Name" v-model="createRoomInfo.name" :error-messages="this.createRoomInfo.error"></v-text-field>
-          <v-text-field
-              v-if="createRoomInfo.protected"
-              label="Password"
-              v-model="createRoomInfo.password"
-              type="password"
-              bg-color="background"
-          ></v-text-field>
-          <v-switch v-model="createRoomInfo.protected" label="Protected" color="indigo"></v-switch>
-          <v-switch v-model="createRoomInfo.private" label="Private" color="indigo"></v-switch>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn :color="isCreateButtonClickable ? 'primary' : 'disabled'" @click="createRoom" :disabled="!isCreateButtonClickable">
-            Create
-          </v-btn>
-          <v-btn color="error" @click="resetCreateForm" bg-color="background">
-            Cancel
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <!--    End of Pop up -->
-    <v-btn color="background" variant="tonal" @click="openJoinRoomDialog">
-      Join a Chat Room
-    </v-btn>
-    <!--    Pop up join chat room -->
-    <v-dialog v-model="joinRoomDialog" max-width="500px">
-      <v-card>
-        <v-card-title>Join Chat Room</v-card-title>
-        <v-card-text>
-          <v-text-field label="Chatroom Name" v-model="joinRoomInfo.roomName" :error-messages="error"></v-text-field>
-          <v-text-field
-              label="Password"
-              v-model="joinRoomInfo.password"
-              type="password"
-          ></v-text-field>
-        </v-card-text>
-        <v-card-actions>
-          <v-btn :color="isJoinButtonClickable ? 'primary' : 'disabled'" @click="joinRoom" :disabled="!isJoinButtonClickable">
-            Join
-          </v-btn>
-          <v-btn color="error" @click="resetJoinForm">Cancel</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </header>
+  <v-dialog v-model="dialogs.password" max-width="500px">
+    <v-card>
+      <v-card-title>Protected Room</v-card-title>
+      <v-card-text>
+        <v-text-field
+            label="Password"
+            v-model="joinRoomInfo.password"
+            type="password"
+            :error-messages="error"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn :color="isJoinButtonClickable ? 'primary' : 'disabled'" @click="joinRoom" :disabled="!isJoinButtonClickable">
+          Join
+        </v-btn>
+        <v-btn color="error" @click="resetJoinForm">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <v-btn color='background' variant="tonal" @click="openJoinRoomDialog">
+    Join a Chat Room
+  </v-btn>  <v-dialog v-model="dialogs.join" max-width="500px">
+    <v-card>
+      <v-card-title>Join Chat Room</v-card-title>
+      <v-card-text>
+        <v-text-field label="Chatroom Name" v-model="joinRoomInfo.roomName" :error-messages="error"></v-text-field>
+        <v-text-field
+            label="Password"
+            v-model="joinRoomInfo.password"
+            type="password"
+        ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn :color="isJoinButtonClickable ? 'primary' : 'disabled'" @click="joinRoom" :disabled="!isJoinButtonClickable">
+          Join
+        </v-btn>
+        <v-btn color="error" @click="resetJoinForm">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+  <v-btn color='background' variant="tonal" @click="openCreateRoomDialog">
+    Create Chat Room
+  </v-btn>
+  <v-dialog v-model="dialogs.create" max-width="500px">
+    <v-card>
+      <v-card-title>Create Chat Room</v-card-title>
+      <v-card-text>
+        <v-text-field label="Chatroom Name" v-model="createRoomInfo.name" :error-messages="this.createRoomInfo.error"></v-text-field>
+        <v-text-field
+            v-if="createRoomInfo.protected"
+            label="Password"
+            v-model="createRoomInfo.password"
+            type="password"
+        ></v-text-field>
+        <v-switch v-model="createRoomInfo.protected" label="Protected" color="indigo"></v-switch>
+        <v-switch v-model="createRoomInfo.private" label="Private" color="indigo"></v-switch>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn :color="isCreateButtonClickable ? 'primary' : 'disabled'" @click="createRoom" :disabled="!isCreateButtonClickable">
+          Create
+        </v-btn>
+        <v-btn color="error" @click="resetCreateForm">Cancel</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
   <v-text-field bg-color='background' label="Message" v-model="message.content" @keyup.enter="sendMessage"></v-text-field>
   <v-card class="message-container" flat>
-    <v-card-text class="flex-grow-1 overflow-y-auto">
-      <template v-for="(msg, i) in chatRoomMessages" :key="i">
-        <div :class="{ 'd-flex flex-row-reverse': isMyMessage(msg) }">
-          <v-hover v-slot:default="{ hover }">
-            <v-chip :color="isMyMessage(msg) ? 'primary' : ''" dark style="height:auto;white-space: normal;" class="pa-4 mb-2">
-              {{ msg.content }}
-              <sub class="ml-2" style="font-size: 0.5rem;">{{ formatMessageDate(msg.date, true) }}</sub>
-              <v-icon v-if="hover" small>expand_more</v-icon>
-            </v-chip>
-          </v-hover>
-        </div>
-      </template>
+    <v-card-text ref="messageContainer" class="flex-grow-1 overflow-y-auto">
+      <v-virtual-scroll :items="chatRoomMessages" height="400">
+        <template v-slot:default="{ item: msg }">
+          <div :class="{ 'd-flex flex-row-reverse': isMyMessage(msg) }">
+            <v-hover v-slot:default="{ hover }">
+              <v-chip :color="isMyMessage(msg) ? 'primary' : ''" dark style="height:auto;white-space: normal;" class="pa-4 mb-2">
+                {{ msg.content }}
+                <sub class="ml-2" style="font-size: 0.5rem;">{{ formatMessageDate(msg.timestamp, true) }}</sub>
+                <v-icon v-if="hover" small>expand_more</v-icon>
+              </v-chip>
+            </v-hover>
+          </div>
+        </template>
+      </v-virtual-scroll>
     </v-card-text>
   </v-card>
-  <v-layout>
-    <v-navigation-drawer expand-on-hover rail location="right">
-      <template v-slot:prepend>
-        <v-list-item
-            v-for="member in chatRoomMembers"
-            :key="member.id"
-            lines="two"
-            :prepend-avatar="member.profile.avatar"
-            :title="member.profile.name"
-            :subtitle="member.profile.status"
-        ></v-list-item>
-      </template>
-    </v-navigation-drawer>
-    <v-main style="height: 250px"></v-main>
-  </v-layout>
+
+<!--  <v-layout>-->
+<!--    <v-navigation-drawer expand-on-hover rail location="right">-->
+<!--      <template v-slot:prepend>-->
+<!--        <v-list-item-->
+<!--            v-for="member in chatRoomMembers"-->
+<!--            :key="member.id"-->
+<!--            lines="two"-->
+<!--            :prepend-avatar="member.profile.avatar"-->
+<!--            :title="member.profile.name"-->
+<!--            :subtitle="member.profile.status"-->
+<!--        ></v-list-item>-->
+<!--      </template>-->
+<!--    </v-navigation-drawer>-->
+<!--    <v-main style="height: 250px"></v-main>-->
+<!--  </v-layout>-->
 </template>
 
 <script lang="ts">
 
+import { defineComponent } from 'vue'
 import useAuthStore from "@/stores/AuthStore.ts";
 import axios from '@/utils/axios';
+import useChatStore from "@/stores/ChatStore";
+import chatSocketService from "@/utils/socketio";
 
 const authStore = useAuthStore()
+const socket = chatSocketService
 const socketOptions = {
   transportOptions: {
     polling: {
@@ -117,63 +138,79 @@ const socketOptions = {
     }
   }
 };
-export default {
+export default defineComponent({
   name: 'ChatRoom-View',
   data() {
     return {
+      select: {id: 0, name: 'General'},
       user: authStore.user,
       message: {room: '', content: ''},
-      chatRooms: [{name: ''}],
-      currentRoom: {name: ''},
+      chatRooms: [],
+      currentRoom: {id: 0, name: 'General'},
+      oldRoom: {id: 0, name: 'General'},
       chatRoomMembers: [],
       chatRoomMessages: [],
-      createRoomDialog: false,
+      dialogs: {join: false, create: false, password: false},
       createRoomInfo: {ownerId: authStore.user.id, name: '', password: '', private: false, protected: false},
-      joinRoomDialog: false,
       joinRoomInfo: {userId: authStore.user.id, roomName: '', password: ''},
       error: ''
     }
   },
   beforeCreate() {
-    // this.$chatSocket.setupSocketConnection(socketOptions)
-    // this.$chatSocket.socket.on('add-member', (member: any) => {this.addMember(member);});
-    // this.$chatSocket.socket.on('chat', (message: any) => {this.addMessage(message);});
+    socket.connectChat(socketOptions)
+    socket.socket.on('message', (message: any) => {
+      this.addMessage(message);}
+    );
   },
   created() {
-    // this.$chatSocket.socket.on('chat', (message: any) => {this.addMessage(message);})
-  },
-  mounted() {
-    // this.chatRoomSetUp('General')
-    this.setRooms()
+    this.getRooms()
+    this.getRoomMessages()
   },
   beforeUnmount() {
-    // send chatroomMember
-    // this.$chatSocket.disconnect();
+    socket.disconnect()
   },
   watch: {
-    'currentRoom.name': function(newRoom, oldRoom) {
-      this.changeRoom(newRoom, oldRoom)
-    },
+    select(newValue, oldValue) {
+      if (newValue.name === 'General') {
+        this.currentRoom = newValue
+        return
+      }
+      this.joinRoomInfo.roomName = newValue.name
+      if(newValue.protected) {
+        this.openPasswordDialog()
+      } else {
+        this.joinRoom()
+      }
+    }
   },
   computed: {
     isCreateButtonClickable() {
       return !(!this.createRoom.name.trim() || (this.createRoom.protected && !this.createRoom.password.trim()))
     },
     isJoinButtonClickable() {
+      if (this.currentRoom.protected && !this.joinRoomInfo.password) {
+        return false;
+      }
       return !(!this.joinRoom.name.trim())
     },
   },
   methods: {
-    changeRoom(newRoom, oldRoom) {
-      // this.$chatSocket.socket.emit('leaveRoom', oldRoom)
-      if (newRoom === 'General') {
-        // this.$chatSocket.socket.emit('joinRoom', oldRoom)
-      } else {
-        this.joinRoomInfo.roomName = newRoom
-        this.openJoinRoomDialog()
+    async getRoomMessages() {
+      if (this.chatRoomMessages) {
+        this.chatRoomMessages.splice(0)
+      }
+      try {
+        const { data } = await axios.get("/chat/messages/" + this.currentRoom.id, { params: { take: 100 }})
+        this.chatRoomMessages.push(...data)
+      } catch (e) {
+        // to think about
       }
     },
-    async setRooms() {
+    async getRooms() {
+      if (this.chatRooms) {
+        this.chatRooms.splice(0)
+      }
+      this.chatRooms.push({id:0, name: 'General'})
       try {
         const { data } = await axios.get("/chat/rooms")
         this.chatRooms.push(...data)
@@ -181,23 +218,21 @@ export default {
         // to think about
       }
     },
-    sendInfo(chat, clientId) {
-      chat.emit('add-member', authStore.user)
-    },
-    addMember(newMember) {
-      const index = this.chatRoomMembers.findIndex(member => member.id == newMember.id)
-      if (index !== -1) {return }
-      this.chatRoomMembers.push(newMember)
-    },
-    openCreateRoomDialog() {this.createRoomDialog = true},
+    // async setRoomMembers() {
+    //   if (this.chatRooms) {
+    //     this.chatRoomMembers.splice(0)
+    //   }
+    //   try {
+    //     const { data } = await axios.get("/chat/rooms/" + this.currentRoom.id)
+    //     this.chatRoomMembers.push(...data)
+    //   } catch (e) {
+    //     // to think about
+    //   }
+    // },
+    openCreateRoomDialog() {this.dialogs.create = true},
     resetCreateForm() {
-      this.createRoomDialog = false
+      this.dialogs.create = false
       this.createRoomInfo = {ownerId: authStore.user.id, name: '', password: '', private: false, protected: false}
-      this.error = ''
-    },
-    resetJoinForm() {
-      this.joinRoomDialog = false
-      this.joinRoomInfo = {userId: authStore.user.id, roomName: '', password: ''}
       this.error = ''
     },
     async createRoom() {
@@ -217,9 +252,19 @@ export default {
         }
       }
     },
-    openJoinRoomDialog() {this.joinRoomDialog = true},
+    openPasswordDialog() {this.dialogs.password = true},
+    openJoinRoomDialog() {this.dialogs.join = true},
+    resetJoinForm() {
+      this.dialogs.password = false
+      this.dialogs.join = false
+      this.joinRoomInfo = {userId: authStore.user.id, roomName: '', password: ''}
+      this.error = ''
+    },
     async joinRoom() {
-      if (!this.isJoinButtonClickable) {return;}
+      console.log(this.joinRoomInfo)
+      if (!this.isJoinButtonClickable) {
+        return;
+      }
       try {
         await axios.post('/chat/join', this.joinRoomInfo);
         // this.$chatSocket.emit('joinRoom', this.joinRoom.name)
@@ -229,29 +274,17 @@ export default {
         this.error = [error.response.data.message];
       }
     },
-    async chatRoomSetUp(room) {
-      if(room == 'General') {
-        // this.$chatSocket.socket.on('general', (clientId: number) => {this.sendInfo(this.$chatSocket.socket, clientId);});
-        // this.$chatSocket.socket.emit('general')
-        console.log('do something special')
-      }
-      try {
-        // const { data } = await axios.get("/chat/room/messages/" + room)
-        // this.chatRoomMessages.push(...data)
-      } catch (e) {
-        // to think about
-      }
-    },
     sendMessage(){
-      this.message.room = this.currentRoom.name;
-      // this.$chatSocket.socket.emit('chat', this.message);
+      if (!this.message.content) {
+        return
+      }
+      socket.socket.emit('message', this.message.content);
       this.message.content = '';
     },
     addMessage(message: any) {
-      console.log(message)
       this.chatRoomMessages.push(message)
     },
-    isMyMessage(msg: any) : boolean {return msg.username === this.user.username;},
+    isMyMessage(msg: any) : boolean {return msg.userId === this.user.id;},
     formatMessageDate(date, includeTime = false) {
       const userTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
       const options = {
@@ -260,7 +293,6 @@ export default {
         month: 'long',
         day: 'numeric',
       };
-
       if (includeTime) {
         options.hour = 'numeric';
         options.minute = 'numeric';
@@ -269,8 +301,7 @@ export default {
       return new Date(date).toLocaleString('en-US', options);
     },
   },
-}
-
+})
 </script>
 
 <style>
@@ -278,8 +309,8 @@ export default {
   display: flex;
   align-items: center;
 }
-.black-text-field {
-  color: black;
+.message-container {
+  height: 400px;
 }
 
 
