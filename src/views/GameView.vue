30377c9995055d27ed2d7dc533e2193c5a827f32
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-30 15:58:41 -0400

    update game invite logic

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -64,16 +64,7 @@
       :debugMode="debugMode"
     />
   </div>
-  <v-dialog v-model="inviteDialog" max-width="400">
-    <v-card>
-      <v-card-title>You received an invite to play Pong</v-card-title>
-      <v-card-title>From: {{ invite.username }}</v-card-title>
-      <v-card-actions>
-        <v-btn color="primary" @click="acceptInvite">Accept</v-btn>
-        <v-btn color="primary" @click="rejectInvite">Cancel</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <invite-dialog v-model="globalStore.dialogs.invite" />
 </template>
 
 <script lang="ts">
@@ -82,7 +73,8 @@ import {GameDataI} from '@/Game/pong-scenes/Assets'
 import {GameUser, GameUserType} from '@/Game/network/GameNetwork'
 import useAuthStore from '@/stores/AuthStore'
 import useGameStore from '@/stores/GameStore'
-import chatSocketService from "@/utils/socketio";
+import useGlobalStore from "@/stores/GlobalStore"
+import chatSocketService from "@/utils/socketio"
 
 const PongGamePlayer = defineAsyncComponent(() => import('@/components/PongGamePlayer.vue'))
 const socket = chatSocketService
@@ -93,16 +85,8 @@ export default defineComponent({
   setup() {
     const authStore = useAuthStore()
     const gameStore = useGameStore()
-    const socketOptions = {
-      transportOptions: {
-        polling: {
-          extraHeaders: {
-            authorization: 'Bearer ' + authStore.token
-          }
-        }
-      }
-    };
-    return { authStore, gameStore, socketOptions }
+    const globalStore = useGlobalStore()
+    return { authStore, gameStore, globalStore }
   },
   data() {
     const gameData: GameDataI = {
@@ -127,7 +111,8 @@ export default defineComponent({
     }
   },
   beforeCreate() {
-    socket.connectChat(this.socketOptions)
+    this.globalStore.connectSocket()
+    this.globalStore.listenGameInvite()
   },
   beforeMount() {
     const currentUser = this.authStore.getUser
@@ -141,36 +126,10 @@ export default defineComponent({
       this.$router.push({ name: 'auth' })
     }
   },
-  mounted() {
-    socket.socket.emit('game')
-    socket.socket.on('game-invite', (user) => {
-      if (this.inviteDialog == false) {
-        this.showInvite(user)
-      }
-    })
-    socket.socket.on('game-accept', () => {this.acceptInvite()})
-    socket.socket.on('game-reject', () => {this.rejectInvite()})
-    if (this.gameStore.getInvited == true) {
-      this.gameStore.setInvited(false)
-      this.playOnline()
-    }
-  },
   beforeUnmount() {
-    socket.disconnect()
+    this.globalStore.disconnectSocket()
   },
   methods: {
-    showInvite(user) {
-      this.invite = user
-      this.inviteDialog = true },
-    acceptInvite() {
-      socket.socket.emit('game-accept', this.invite.username)
-      this.inviteDialog = false
-      this.playOnline()
-    },
-    rejectInvite() {
-      socket.socket.emit('game-reject', this.invite.username)
-      this.inviteDialog = false
-    },
     playLocally() {
       this.gameData.playerType = GameUserType.LocalPlayer
       this.gamePlayerKey++
