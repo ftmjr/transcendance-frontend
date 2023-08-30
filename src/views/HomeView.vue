a9ee7e46920772b1ad2b3a63faf09bda899c8fc2
Francis Lahoud <flahoud@student.42quebec.com>
2023-08-29 22:58:26 -0400

    added stores if we want to receive game invite on home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <template>
+  <invite-dialog v-model="globalStore.showInvite" />
   <div class="container">
     <p class="text-white text-md">Dashboard home view</p>
   </div>
@@ -7,16 +8,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import useAuthStore from '@/stores/AuthStore'
+import useGlobalStore from "@/stores/GlobalStore";
+import InviteDialog from "@/components/chat/InviteDialog.vue";
 
 export default defineComponent({
   name: 'DashBoard',
-  components: {},
+  components: {InviteDialog},
   setup() {
+    const globalStore = useGlobalStore()
     const authStore = useAuthStore()
-    return { authStore }
+    return { authStore, globalStore }
   },
-  data() {
-    return {}
-  }
 })
 </script>
