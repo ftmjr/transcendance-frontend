d36246b1c06fa556576d022f8c79815b40175290
Josias Kasongo <josiaskas@gmail.com>
2023-09-04 23:04:55 -0400

    add config for vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import vuetify from '@/utils/vuetify'
+import vuetify from '@/vuetify/config'
+import layoutsPlugin from '@/vuetify/layouts'
 import App from './App.vue'
 import router from './router'
-
+import '@core/scss/template/index.scss'
 import './assets/main.scss'
-
 const app = createApp(App)
 
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
+app.use(layoutsPlugin)
 app.mount('#app')
