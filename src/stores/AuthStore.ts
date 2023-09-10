caba26c49e5f8a86de90ee2a741201cd1feaae2b
Josias Kasongo <josiaskas@gmail.com>
2023-09-10 16:29:21 -0400

    change auth logic approach, jwt can tell if we need to lock the user

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,19 +1,43 @@
 import { defineStore } from 'pinia'
-import router from '@/router'
-import type { AuthState, User, RegisterBody, ILoginData } from 'Auth'
+import type { AuthState, ILoginData, RegisterBody, User, Profile } from 'Auth'
 import type { AxiosError } from 'axios'
 import axios from '@/utils/axios'
+
+interface JwtPayload {
+  email: string
+  sub: {
+    userId: number
+    sessionId: number
+    needToPassTwoFactor: boolean
+  }
+  exp: number
+  iat: number
+}
+export enum LoginStatus {
+  NOT_LOGGED = 'notLogged',
+  LOGGED = 'logged',
+  TWOFA_CHECK = '2faCheck',
+  LOCKED = 'locked'
+}
+function decodeJWT(token): JwtPayload | null {
+  try {
+    const payloadBase64 = token.split('.')[1]
+    const decodedPayload = atob(payloadBase64)
+    const decoded = JSON.parse(decodedPayload)
+    if (!decoded || !decoded.exp) throw new Error('Invalid token')
+    return decoded
+  } catch (e) {
+    return null // Or handle the error as you see fit
+  }
+}
 
 const useAuthStore = defineStore({
   id: 'auth',
   state: (): AuthState => {
-    const token = localStorage.getItem('__token__')
-    const expiry = localStorage.getItem('__tokenExpiry__')
-    const tokenExpiry: number = expiry ? parseInt(expiry) : 0
+    const token = localStorage.getItem('__token__') || null
     const user = JSON.parse(localStorage.getItem('__user__') ?? 'null') as User | null
     return {
       token,
-      tokenExpiry,
       user,
       error: {
         state: false,
@@ -22,82 +46,70 @@ const useAuthStore = defineStore({
     }
   },
   getters: {
-    getToken(): string | null {
-      return this.token
+    getTokenData(): JwtPayload | null {
+      return decodeJWT(this.token)
     },
-    getAuthError(): { state: boolean; message: string } {
-        return this.error
+    isExpired() {
+      return this.getTokenData?.exp < Math.floor(Date.now() / 1000)
     },
-    isExpired(): boolean {
-      return Date.now() > this.tokenExpiry
+    status(): LoginStatus {
+      if (this.user) {
+        if (this.token) {
+          if (this.isExpired) {
+            return LoginStatus.LOCKED
+          }
+
+          const decoded = this.getTokenData
+          if (!decoded) {
+            return LoginStatus.LOCKED
+          }
+
+          if (decoded.sub.needToPassTwoFactor) {
+            return LoginStatus.TWOFA_CHECK
+          } else {
+            return LoginStatus.LOGGED
+          }
+        } else {
+          return LoginStatus.LOCKED
+        }
+      }
+      return LoginStatus.NOT_LOGGED
+    },
+    isLoggedIn() {
+      return this.status === LoginStatus.LOGGED
+    },
+    is2FA() {
+      return this.status === LoginStatus.TWOFA_CHECK
+    },
+    isLocked() {
+      return this.status === LoginStatus.LOCKED
     },
     getUser(): User | null {
       return this.user
     },
-    getProfile() {
-      return this.user.profile
-    },
-    getAvatar() {
-      return this.user.profile.avatar
-    },
-    isAuthenticated(): boolean {
-      return !!this.token
+    getProfile(): Profile | null {
+      return this.getUser?.profile ?? null
     }
   },
   actions: {
-    async refreshUser() {
-      try {
-        const { data } = await axios.get('auth/me')
-        this.setUser(data)
-      } catch (error) {
-        // to think about
-      }
-    },
-    setToken(token: string) {
-      this.token = token
-      localStorage.setItem('__token__', token)
-      this.tokenExpiry = JSON.parse(atob(token.split('.')[1])).exp * 1000
-      localStorage.setItem('__tokenExpiry__', this.tokenExpiry.toString())
-      this.scheduleTokenRefresh()
-    },
-    scheduleTokenRefresh() {
-      const refreshAt = this.tokenExpiry - 30 * 1000
-      const delay = refreshAt - Date.now()
-      if (delay > 0) {
-        // console.log('scheduling token refresh', delay)
-        setTimeout(() => {
-          this.refreshToken()
-        }, delay)
-      }
-    },
     setUser(user: User) {
       this.user = user
       localStorage.setItem('__user__', JSON.stringify(user))
     },
-    setError(error: { state: boolean; message: string }) {
-      this.error = error
+    setToken(token: string) {
+      this.token = token
+      localStorage.setItem('__token__', token)
     },
     removeToken() {
-      localStorage.removeItem('__token__')
-      localStorage.removeItem('__tokenExpiry__')
       this.token = null
+      localStorage.removeItem('__token__')
     },
     removeUser() {
       this.user = null
       localStorage.removeItem('__user__')
     },
-    async fetchUser(): Promise<boolean> {
-      try {
-        const { data } = await axios.get('auth/me')
-        this.setUser(data)
-        return true
-      } catch (e: unknown) {
-        this.removeToken()
-        this.removeUser()
-        return false
-      }
-    },
     async login(credentials: { username: string; password: string }): Promise<boolean> {
+      this.error = { state: false, message: '' }
       try {
         const { data } = await axios.post('auth/login', credentials, {
           headers: {
@@ -108,26 +120,26 @@ const useAuthStore = defineStore({
         const { accessToken, user } = data as ILoginData
         this.setUser(user)
         this.setToken(accessToken)
-        this.setError({ state: false, message: '' })
         return true
       } catch (error: AxiosError | any) {
         if (error.response && error.response.status === 401) {
-          this.setError({ state: true, message: error.response.data.message })
-          return false
+          this.error = { state: true, message: error.response.data.message }
         }
       }
+      return false
     },
     async logout() {
+      this.error = { state: false, message: '' }
       try {
         await axios.get('auth/logout')
       } catch (error: AxiosError | any) {
-        this.setError({ state: true, message: error.response.data.message })
+        this.error = { state: true, message: error.response.data.message }
       }
       this.removeToken()
       this.removeUser()
-      await router.push({ name: 'auth' })
     },
     async register(userInfos: RegisterBody): Promise<boolean> {
+      this.error = { state: false, message: '' }
       if (!userInfos || userInfos.password !== userInfos.passwordConfirmation) return false
       const { passwordConfirmation, ...body } = userInfos
       try {
@@ -139,17 +151,16 @@ const useAuthStore = defineStore({
         const { accessToken, user } = data as ILoginData
         this.setUser(user)
         this.setToken(accessToken)
-        this.setError({ state: false, message: '' })
         return true
       } catch (error: AxiosError | any) {
         if (error.response && error.response.status === 403) {
-          // get the backend error msg
-          this.setError({ state: true, message: error.response.data.message })
+          this.error = { state: true, message: error.response.data.message }
         }
-        return false
       }
+      return false
     },
     async refreshToken(): Promise<string | null> {
+      this.error = { state: false, message: '' }
       try {
         if (!this.token) return null
         const { data } = await axios.get('/auth/refresh', {
@@ -159,105 +170,101 @@ const useAuthStore = defineStore({
         })
         const { accessToken } = data as { accessToken: string }
         this.setToken(accessToken)
-        this.setError({ state: false, message: '' })
         return accessToken
       } catch (error: AxiosError | any) {
         if (error.response && error.response.status === 401) {
           await this.logout()
-          this.setError({ state: true, message: error.response.data.message })
+          this.error = { state: true, message: error.response.data.message }
         } else {
-          this.setError({ state: true, message: 'Failed to refresh token' })
+          this.error = { state: true, message: 'Failed to refresh token' }
         }
-        return null
       }
+      return null
     },
-    // first usage
     async generate2FAQrCode(): Promise<string | null> {
+      this.error = { state: false, message: '' }
       try {
         const { data } = await axios.post('auth/2fa/generate')
         return data as string
       } catch (error) {
-        this.setError({ state: true, message: `Can't generate 2FA code` })
+        this.error = { state: true, message: `Can't generate 2FA code` }
         return null
       }
     },
-    // every validation if 2FA is enabled
-    async validate2FACode(totpCode: string): Promise<boolean> {
-      try {
-        const { data } = await axios.post('auth/2fa/authenticate', {
-          twoFactorAuthenticationCode: totpCode
-        })
-        return data as boolean
-      } catch (error) {
-        this.setError({ state: true, message: `Can't validate OTP code` })
-        return false
-      }
-    },
-    // subsequent usage when activating 2FA
     async activate2FA(otpCode: string): Promise<boolean> {
+      this.error = { state: false, message: '' }
       try {
         if (!this.user.twoFactorEnabled) {
           await axios.post('auth/2fa/turn-on', {
             twoFactorAuthenticationCode: otpCode
           })
-          await this.fetchUser();
+          await this.fetchUser()
         }
         return true
       } catch (error) {
-        this.setError({ state: true, message: `Can't validate OTP code` })
+        this.error = { state: true, message: `Can't validate OTP code` }
+        return false
+      }
+    },
+    async validate2FACode(totpCode: string): Promise<boolean> {
+      this.error = { state: false, message: '' }
+      try {
+        const { data } = await axios.post('auth/2fa/authenticate', {
+          twoFactorAuthenticationCode: totpCode
+        })
+        await this.refreshToken()
+        return data as boolean
+      } catch (error) {
+        this.error = { state: true, message: `Can't validate OTP code` }
         return false
       }
     },
     async deactivate2FA(): Promise<boolean> {
+      this.error = { state: false, message: '' }
       try {
-        this.setError({ state: false, message: '' })
         await axios.get('auth/2fa/turn-off')
-        await this.fetchUser();
-        return true;
+        await this.fetchUser()
+        return true
       } catch (error) {
-        this.setError({ state: true, message: `Can't deactivate 2FA` })
+        this.error = { state: true, message: `Can't deactivate 2FA` }
         return false
       }
     },
-    async updatePassword(info:{
-      currentPassword: string;
-      newPassword: string;
-      confirmPassword: string;
+    async updatePassword(info: {
+      currentPassword: string
+      newPassword: string
+      confirmPassword: string
     }): Promise<boolean> {
-      this.setError({ state: false, message: '' });
+      this.error = { state: false, message: '' }
       try {
         const { data } = await axios.post('auth/updatePassword', info)
-        this.setUser(data);
-        return true;
+        this.setUser(data)
+        return true
       } catch (error) {
         if (error.response && (error.response.status === 403 || error.response.status === 401)) {
-          const message = JSON.stringify(error.response.data.message ?? 'error');
-          this.setError({ state: true, message })
+          const message = JSON.stringify(error.response.data.message ?? 'error')
+          this.error = { state: true, message }
         } else {
-          this.setError({ state: true, message: `Can't update password` })
+          this.error = { state: true, message: `Can't update password` }
         }
         return false
       }
     },
-    async updateUserInfo(info:{
-      firstName: string;
-      lastName: string;
-      bio: string;
-    }){
-      this.setError({ state: false, message: '' });
+    async updateUserInfo(info: { firstName: string; lastName: string; bio: string }) {
+      this.error = { state: false, message: '' }
       try {
         const { data } = await axios.post('auth/updateInfo', info)
-        this.setUser(data);
-        return true;
+        this.setUser(data)
+        return true
       } catch (error) {
         if (error.response && (error.response.status === 403 || error.response.status === 401)) {
-            this.setError({ state: true, message: error.response.data.message })
-        }else{
-            this.setError({ state: true, message: `Can't update user info` })
+          this.error = { state: true, message: error.response.data.message }
+        } else {
+          this.error = { state: true, message: `Can't update user info` }
         }
         return false
       }
-    },
+    }
   }
 })
 
