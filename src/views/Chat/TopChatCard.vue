68803fd42719960b37ceba68b79a98e78265be2c
Josias Kasongo <josiaskas@gmail.com>
2023-09-29 23:29:13 -0400

    start working on notifications

--- src/views/Chat/TopChatCard.vue
+++ src/views/Chat/TopChatCard.vue
@@ -1,7 +1,7 @@
 <template>
   <VCard color="#952175" :loading="loading" src="./public/pong/backgrounds/pink_big_bg.png">
     <template v-slot:loader="{ isActive }">
-      <v-progress-linear :active="isActive" color="blue" height="4" :indeterminate="true" />
+      <v-progress-linear :active="isActive" color="blue" height="64" :indeterminate="true" />
     </template>
     <VCarousel
       cycle
@@ -9,24 +9,49 @@
       :show-arrows="false"
       hide-delimiter-background
       :delimiter-icon="() => h(VIcon, { icon: 'fa-circle', size: '10' })"
-      height="auto"
-      class=""
+      height="320"
     >
       <VCarouselItem v-for="(room, index) in topChatsRooms" :key="room.id">
-        <VCardText>
-          <VRow>
+        <VCardText class="h-full">
+          <VRow class="h-25">
             <VCol cols="12">
-              <h6 class="font-weight-semibold text-white mb-1">Les Top Chat - {{ index + 1 }}</h6>
+              <h6 class="font-weight-semibold text-white">Les Top Chat - {{ index + 1 }}</h6>
             </VCol>
             <VCol cols="12" sm="6" order="2" order-sm="1">
               <VRow>
                 <VCol cols="12" class="pb-0">
                   <p class="text-xl font-weight-semibold">{{ room.name }}</p>
                 </VCol>
+                <VCol cols="6" class="text-no-wrap">
+                  <VChip label class="me-2">
+                    {{ room.members.length }}
+                  </VChip>
+                  <span>Membres(s)</span>
+                </VCol>
+                <VCol cols="6" class="text-no-wrap">
+                  <VChip label class="me-2">
+                    {{ room.type }}
+                  </VChip>
+                </VCol>
+                <VCol cols="12" class="pt-2 items-center justify-center">
+                  <VBtn
+                    v-if="!isCurrentUserAMember(room)"
+                    color="white"
+                    class="text-no-wrap"
+                    @click="joinRoom(room.id)"
+                  >
+                    <VIcon left>mdi-account-plus</VIcon>
+                    <span>Rejoindre</span>
+                  </VBtn>
+                  <VBtn v-else color="white" @click="goToChatRoom(room.id)">
+                    <VIcon left>tabler-eye</VIcon>
+                    <span>Voir</span>
+                  </VBtn>
+                </VCol>
               </VRow>
             </VCol>
-            <VCol cols="12" sm="6" order="1" order-sm="2" class="position-relative text-center">
-              <img :src="room.avatar" class="inset-1.5 rounded" />
+            <VCol cols="12" sm="6" order="1" order-sm="2">
+              <img :src="room.avatar" class="rounded object-cover" />
             </VCol>
           </VRow>
         </VCardText>
@@ -40,9 +65,16 @@ import { defineComponent, h } from 'vue'
 import { VIcon } from 'vuetify/components'
 import axiosInstance from '@/utils/axios'
 import { ChatRoom } from '@/utils/chatSocket'
+import useAuthStore from '@/stores/AuthStore'
 
 export default defineComponent({
   name: 'TopChatCard',
+  setup() {
+    const authStore = useAuthStore()
+    return {
+      authStore
+    }
+  },
   data() {
     return {
       loading: false,
@@ -74,6 +106,15 @@ export default defineComponent({
         console.log(e)
       }
       this.loading = false
+    },
+    isCurrentUserAMember(room: ChatRoom) {
+      return room.members.some((member) => member.id === this.authStore.getUser.id)
+    },
+    goToChatRoom(roomId: number) {
+      // go to chat room
+    },
+    joinRoom(roomId: number) {
+      // join room
     }
   }
 })
