c5d47bdb0b2aa6bef717c431dff89c1be942012d
Josias Kasongo <josiaskas@gmail.com>
2023-09-30 20:47:14 -0400

    move template on to be consistent.

--- src/views/User/Settings.vue
+++ src/views/User/Settings.vue
@@ -1,3 +1,106 @@
+<template>
+  <div>
+    <VTabs v-model="activeTab" class="v-tabs-pill">
+      <VTab
+          v-for="item in tabs"
+          :key="item.icon"
+          :value="item.tab"
+          :to="{ name: 'settings', params: { tab: item.tab } }"
+      >
+        <VIcon size="20" start :icon="item.icon" />
+        {{ item.title }}
+      </VTab>
+    </VTabs>
+    <VWindow v-model="activeTab" class="mt-6 disable-tab-transition" :touch="false">
+      <VWindowItem value="account">
+        <VRow>
+          <VCol cols="12">
+            <VCard title="Information du profil">
+              <VCardText class="flex">
+                <VAvatar rounded size="100" class="me-6" :image="avatar" />
+                <form
+                    ref="refUpdateAvatarForm"
+                    class="flex flex-column justify-center gap-4"
+                    @submit.prevent
+                >
+                  <div class="flex flex-wrap gap-2">
+                    <VBtn color="primary" @click="inputElement?.click()">
+                      <VIcon icon="tabler-cloud-upload" class="d-sm-none" />
+                      <span class="d-none d-sm-block">Changer d'avatar</span>
+                    </VBtn>
+
+                    <input
+                        ref="refInputEl"
+                        type="file"
+                        name="file"
+                        accept=".jpeg,.png,.jpg,GIF"
+                        hidden
+                        @input="startUploadNewAvatar"
+                    />
+                  </div>
+                  <p class="text-sm font-weight-light mb-0">
+                    Fichier pris en charge: JPG, GIF or PNG
+                  </p>
+                </form>
+              </VCardText>
+
+              <VDivider />
+
+              <VCardText class="pt-2">
+                <VForm class="mt-6" @submit.prevent>
+                  <VRow>
+                    <VCol md="6" cols="12">
+                      <VTextField
+                          v-model="fields.firstName"
+                          label="Prénom"
+                          class="transparent-input-box"
+                      />
+                    </VCol>
+
+                    <VCol md="6" cols="12">
+                      <VTextField
+                          v-model="fields.lastName"
+                          label="Nom de famille"
+                          class="transparent-input-box"
+                      />
+                    </VCol>
+
+                    <VCol cols="12" md="6">
+                      <VTextarea
+                          v-model="fields.bio"
+                          label="Biographie"
+                          class="transparent-input-box"
+                      />
+                    </VCol>
+
+                    <VCol cols="12" class="d-flex flex-wrap gap-4">
+                      <VBtn @click.prevent="updateUserInformation">Enregistrer</VBtn>
+                      <VBtn
+                          color="secondary"
+                          variant="tonal"
+                          type="reset"
+                          @click.prevent="resetForm"
+                      >
+                        Renitialiser
+                      </VBtn>
+                    </VCol>
+                  </VRow>
+                </VForm>
+              </VCardText>
+            </VCard>
+          </VCol>
+        </VRow>
+      </VWindowItem>
+      <VWindowItem value="security">
+        <SecuritySettings />
+      </VWindowItem>
+    </VWindow>
+    <VSnackbar v-model="isInfoBarVisible" multi-line :timeout="1000" :color="infoColor">
+      {{ infoMsg }}
+    </VSnackbar>
+  </div>
+</template>
+
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import SecuritySettings from '@/views/User/SecuritySettings.vue'
@@ -77,108 +180,5 @@ export default defineComponent({
   }
 })
 </script>
-
-<template>
-  <div>
-    <VTabs v-model="activeTab" class="v-tabs-pill">
-      <VTab
-        v-for="item in tabs"
-        :key="item.icon"
-        :value="item.tab"
-        :to="{ name: 'settings', params: { tab: item.tab } }"
-      >
-        <VIcon size="20" start :icon="item.icon" />
-        {{ item.title }}
-      </VTab>
-    </VTabs>
-    <VWindow v-model="activeTab" class="mt-6 disable-tab-transition" :touch="false">
-      <VWindowItem value="account">
-        <VRow>
-          <VCol cols="12">
-            <VCard title="Information du profil">
-              <VCardText class="flex">
-                <VAvatar rounded size="100" class="me-6" :image="avatar" />
-                <form
-                  ref="refUpdateAvatarForm"
-                  class="flex flex-column justify-center gap-4"
-                  @submit.prevent
-                >
-                  <div class="flex flex-wrap gap-2">
-                    <VBtn color="primary" @click="inputElement?.click()">
-                      <VIcon icon="tabler-cloud-upload" class="d-sm-none" />
-                      <span class="d-none d-sm-block">Changer d'avatar</span>
-                    </VBtn>
-
-                    <input
-                      ref="refInputEl"
-                      type="file"
-                      name="file"
-                      accept=".jpeg,.png,.jpg,GIF"
-                      hidden
-                      @input="startUploadNewAvatar"
-                    />
-                  </div>
-                  <p class="text-sm font-weight-light mb-0">
-                    Fichier pris en charge: JPG, GIF or PNG
-                  </p>
-                </form>
-              </VCardText>
-
-              <VDivider />
-
-              <VCardText class="pt-2">
-                <VForm class="mt-6" @submit.prevent>
-                  <VRow>
-                    <VCol md="6" cols="12">
-                      <VTextField
-                        v-model="fields.firstName"
-                        label="Prénom"
-                        class="transparent-input-box"
-                      />
-                    </VCol>
-
-                    <VCol md="6" cols="12">
-                      <VTextField
-                        v-model="fields.lastName"
-                        label="Nom de famille"
-                        class="transparent-input-box"
-                      />
-                    </VCol>
-
-                    <VCol cols="12" md="6">
-                      <VTextarea
-                        v-model="fields.bio"
-                        label="Biographie"
-                        class="transparent-input-box"
-                      />
-                    </VCol>
-
-                    <VCol cols="12" class="d-flex flex-wrap gap-4">
-                      <VBtn @click.prevent="updateUserInformation">Enregistrer</VBtn>
-                      <VBtn
-                        color="secondary"
-                        variant="tonal"
-                        type="reset"
-                        @click.prevent="resetForm"
-                      >
-                        Renitialiser
-                      </VBtn>
-                    </VCol>
-                  </VRow>
-                </VForm>
-              </VCardText>
-            </VCard>
-          </VCol>
-        </VRow>
-      </VWindowItem>
-      <VWindowItem value="security">
-        <SecuritySettings />
-      </VWindowItem>
-    </VWindow>
-    <VSnackbar v-model="isInfoBarVisible" multi-line :timeout="1000" :color="infoColor">
-      {{ infoMsg }}
-    </VSnackbar>
-  </div>
-</template>
 
 <style lang="scss" scoped></style>
