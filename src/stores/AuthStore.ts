102d4405220fc27ac586d264cc85bf1297fad69c
Francis Lahoud <flahoud@student.42quebec.com>
2023-10-07 14:50:08 -0400

    get rid of create account alert

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -149,7 +149,6 @@ const useAuthStore = defineStore({
       }
     },
     async register(userInfos: RegisterBody): Promise<boolean> {
-      alert('register')
       this.error = { state: false, message: '' }
       if (!userInfos || userInfos.password !== userInfos.passwordConfirmation) return false
       const { passwordConfirmation, ...body } = userInfos
