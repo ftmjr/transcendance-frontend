911eff2c9f807680186cea32e665dc176fc73f94
Minsu <50944735+MinsuKin@users.noreply.github.com>
2023-10-14 16:31:27 -0400

    change id to name in the new format

--- src/views/User/Histories.vue
+++ src/views/User/Histories.vue
@@ -1,34 +1,26 @@
 <template>
-  <v-card title="Game Record">
+  <v-card title="Historiques des Actions" :loading="loading">
     <VTable>
       <thead>
       <tr>
-<!--        <th class="text-left">Event</th>-->
-        <th class="text-left">Player</th>
-        <th class="text-left">Competitor</th>
-        <th class="text-left">Score</th>
-        <th class="text-left">Result</th>
-        <th class="text-left">Date</th>
+        <th>Opposant</th>
+        <th>Date</th>
+        <th>Résultat</th>
+        <th>Score</th>
       </tr>
       </thead>
       <tbody>
-      <tr v-for="group in groupedHistories" :key="group.gameId" :class="getTdClass(group.events)">
-<!--        <td>-->
-<!--            <span v-for="(count, event) in group.events" :key="event">-->
-<!--              <v-icon v-if="getIcon(event)">{{ getIcon(event) }}</v-icon>-->
-<!--              <span v-if="count > 1">({{ count }})</span>-->
-<!--            </span>-->
-<!--        </td>-->
-        <td>{{ getUserName(group.userId) }}</td>
-        <td>{{ getUserName(group.competitorId) }}</td>
-        <td>? vs ? //TODO</td>
+      <tr v-for="history in histories" :key="history.gameId">
         <td>
-          <span v-for="(count, event) in group.events" :key="event">
-              <v-icon v-if="getIcon(event)">{{ getIcon(event) }}</v-icon>
-            </span>
-          {{ group.result }}
+          <div>
+            {{ getUserName(getOpponentId(history)) }}
+          </div>
         </td>
-        <td>{{ formatDate(group.latestTimestamp) }}</td>
+        <td>{{ getDate(history) }}</td>
+        <td>{{ getIsUserWon(history) ? 'Victoire' : 'Perdu/Abandon' }}</td>
+        <td>
+          {{ getActions(history) }}
+        </td>
       </tr>
       </tbody>
     </VTable>
@@ -38,30 +30,15 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { formatDate } from '@core/utils/formatters'
-import type { GameHistory } from 'Auth'
+import useGameStore, { CompleteGameHistory, GameSessionType } from '@/stores/GameStore'
 import useUserStore from "@/stores/UserStore";
 import {User} from "Auth";
-import get = Reflect.get;
-
-interface GroupedHistory {
-  gameId: number
-  events: Record<string, number>
-  latestTimestamp: string
-}
+import AvatarBadge from '@/components/profile/AvatarBadge.vue'
 
 // TODO: This component is not finished yet, need to be refatored to display correctly
 export default defineComponent({
-  setup() {
-    const userStore = useUserStore()
-    return {
-      userStore
-    }
-  },
-  data() {
-    return {
-      loading: false,
-      users: [] as User[]
-    }
+  components: {
+    // AvatarBadge
   },
   props: {
     userId: {
@@ -69,48 +46,34 @@ export default defineComponent({
       required: true
     }
   },
-  computed: {
-    groupedHistories(): GroupedHistory[] {
-      const groups: Record<number, GroupedHistory> = {}
-      this.histories.forEach((history: GameHistory) => {
-        if (!groups[history.gameId]) {
-          groups[history.gameId] = {
-            gameId: history.gameId,
-            userId: history.userId,
-            competitorId: history.competitorId,
-            events: {},
-            latestTimestamp: history.timestamp,
-            result: 'Left',
-          }
-        }
-        groups[history.gameId].events[history.event] =
-            (groups[history.gameId].events[history.event] || 0) + 1
-        if (new Date(history.timestamp) > new Date(groups[history.gameId].latestTimestamp)) {
-          groups[history.gameId].latestTimestamp = history.timestamp
-        }
-        if (history.event === 'MATCH_WON') {
-          groups[history.gameId].result = 'Won'
-        } else if (history.event === 'MATCH_LOST') {
-          groups[history.gameId].result = 'Lost'
-        } else if (history.event === 'PLAYER_LEFT') {
-          groups[history.gameId].result = 'Left'
-        }
-      })
-      this.fetchNames()
-      return Object.values(groups).sort((a, b) => b.gameId - a.gameId)
+  setup() {
+    const userStore = useUserStore()
+    const gameStore = useGameStore()
+    return {
+      gameStore,
+      userStore
     }
   },
+  data() {
+    return {
+      histories: [] as CompleteGameHistory[],
+      users: [] as User[],
+      loading: false
+    }
+  },
+  mounted() {
+    this.fetchNames()
+  },
   methods: {
-    get,
-    getIcon(event: string): string | undefined {
-      const icons: Record<string, string> = {
-        //GAME_STARTED: 'tabler-play',
-        //GAME_ENDED: 'tabler-skull',
-        //PLAYER_JOINED: 'tabler-user-plus',
-        PLAYER_LEFT: 'mdi-account-arrow-left',
-        //ACTION_PERFORMED: 'mdi-soccer',
-        MATCH_WON: 'mdi-trophy',
-        MATCH_LOST: 'mdi-skull'
+    formatDate,
+    async fetchHistories() {
+      this.loading = true
+      try {
+        this.histories = await this.gameStore.getUserCompleteGameHistory(this.userId)
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.loading = false
       }
     },
     getGameType(gameHistory: CompleteGameHistory): GameSessionType {
@@ -125,7 +88,52 @@ export default defineComponent({
           return GameSessionType.PrivateGame
       }
     },
-    formatDate,
+    getPlayersIds(gameHistory: CompleteGameHistory): number[] {
+      const ids = Object.keys(gameHistory.histories)
+      return ids.map((id) => parseInt(id, 10))
+    },
+    getOpponentId(gameHistory: CompleteGameHistory): number | null {
+      const gameType = this.getGameType(gameHistory)
+      if (gameType === GameSessionType.Bot) {
+        return 0
+      }
+      // find the first one different from the user
+      const playersId = this.getPlayersIds(gameHistory)
+      const opponentId = playersId.find((id) => id !== this.userId)
+      if (!opponentId) {
+        return null
+      }
+      return opponentId
+    },
+    getDate(gameHistory: CompleteGameHistory): string {
+      // find the last history in the gameHistory
+      const eventOfCurrentUser = Object.values(gameHistory.histories).find((UserHistories) => {
+        return UserHistories.find((userHistory) => userHistory.userId === this.userId)
+      })
+      if (!eventOfCurrentUser) {
+        return ''
+      }
+      const timestamp = eventOfCurrentUser[0].timestamp
+      return formatDate(timestamp)
+    },
+    getIsUserWon(gameHistory: CompleteGameHistory) {
+      const winner = gameHistory.winnerId
+      return winner === this.userId
+    },
+    getActions(gameHistory: CompleteGameHistory): string {
+      //
+      const usersHistories = Object.values(gameHistory.histories)
+      const goals = usersHistories.map((userHistory) => {
+        const goals = userHistory.filter((hist) => hist.event === 'ACTION_PERFORMED')
+        return {
+          userId: userHistory[0].userId,
+          goals: goals.length
+        }
+      })
+      const goalStrings = goals.map((goal) => `${goal.goals}`);
+      const goalsString = goalStrings.join(' vs ');
+      return goalsString;
+    },
     async fetchNames() {
       this.loading = true
       const users = await this.userStore.getAllUsers(100)
@@ -136,6 +144,14 @@ export default defineComponent({
       if (userId === 0) return 'AI BOT'
       const user = this.users.find((user) => user.id === userId)
       return user ? user.username : ''
+    }
+  },
+  watch: {
+    userId: {
+      immediate: true,
+      handler() {
+        this.fetchHistories()
+      }
     }
   }
 })
