eb3576cd89686a643dee2135338e2b80f6cf2d0c
josiaskas <josiaskas@gmail.com>
2023-11-17 12:59:53 -0500

    Avatar badge reactivity update

--- src/components/profile/AvatarBadge.vue
+++ src/components/profile/AvatarBadge.vue
@@ -94,12 +94,8 @@ export default defineComponent({
     }
   },
   data() {
-    let userProfile = null as unknown as ShortUserProfile
-    if (this.user) {
-      userProfile = this.user
-    }
     return {
-      userProfile: userProfile
+      userProfile: null as unknown as ShortUserProfile
     }
   },
   computed: {
@@ -114,16 +110,22 @@ export default defineComponent({
       return this.authStore.resolveAvatarBadgeVariant(this.status)
     }
   },
-  async beforeMount() {
-    if (!this.userProfile) {
-      await this.loadUser(this.userId)
-    }
-  },
   watch: {
     userId: {
       handler(value: number) {
         this.loadUser(value)
       }
+    },
+    user: {
+      handler(value: ShortUserProfile) {
+        if (value) {
+          this.userProfile = value
+        } else{
+          this.loadUser(value)
+        }
+      },
+      deep: true,
+      immediate: true
     }
   },
   methods: {
@@ -131,7 +133,7 @@ export default defineComponent({
     async loadUser(userId: number) {
       const data = await this.usersStore.getShortUserProfile(userId)
       if (data) {
-        this.userProfile = data
+        this.userProfile = data;
       }
     }
   }
